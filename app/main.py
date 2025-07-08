33754d5de03a332e8052ceb09187451ba151483e
Hernan Acero <contacto.acero.hernan@gmail.com>
2025-07-08 00:11:15 -0500

    despliegue en render

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from app.yolo_utils import detectar_residuos
+from yolo_utils import detectar_residuos
 import shutil
 import os
 
